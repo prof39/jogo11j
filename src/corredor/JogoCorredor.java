e7bf5424de96d660230803fdb10dbe4fe56f2ca2
administrador <administrador@mint17-vm>
2016-01-23 20:07:06 +0000

    criação de menu

--- src/corredor/JogoCorredor.java
+++ src/corredor/JogoCorredor.java
@@ -205,6 +205,9 @@ public class JogoCorredor extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(JogoCorredor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
